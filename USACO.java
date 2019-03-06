b261638fca4b1807de56854aeb2ddae3eee216df
Tammy <tchen00@stuy.edu>
2019-03-06 11:33:42 -0500

    read file and make pasture

--- USACO.java
+++ USACO.java
@@ -1,9 +1,17 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
-  public static int silver(){
-    return 0;
+  public static int silver(String input) throws FileNotFoundException{
+    File f = new File(input);
+    Scanner scan = new Scanner(f);
+    String[] info = scan.nextLine().split(" ");
+    int[][] pasture = new int[Integer.parseInt(info[0])][Integer.parseInt(info[1])];
+
+    return 0; 
   }
 
   public static int gold(){
-    return 0; 
+    return 0;
   }
 }

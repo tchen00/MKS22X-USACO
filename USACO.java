e81f0d559ea5aa90c819ef43eff47f587744a06d
Tammy <tchen00@stuy.edu>
2019-03-06 11:42:22 -0500

    add elevations to pasture

--- USACO.java
+++ USACO.java
@@ -5,10 +5,21 @@ public class USACO{
   public static int silver(String input) throws FileNotFoundException{
     File f = new File(input);
     Scanner scan = new Scanner(f);
+
+    // get the first line of the input file
     String[] info = scan.nextLine().split(" ");
+    // using the info just received make the pasture array
     int[][] pasture = new int[Integer.parseInt(info[0])][Integer.parseInt(info[1])];
 
-    return 0; 
+    // add elevations info into the pasture array
+    for (int i = 0; i < pasture.length; i++){
+      String[] elevations = scan.nextLine().split(" ");
+      for (int j = 0; j < elevations.length; j++){
+        pasture[i][j] = Integer.parseInt(elevations[j]);
+      }
+    }
+
+    return 0;
   }
 
   public static int gold(){
